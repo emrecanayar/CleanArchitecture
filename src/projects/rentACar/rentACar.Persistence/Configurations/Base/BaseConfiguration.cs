02f11a980bc53253727ea6db82a67637f018f8b1
Emre Can Ayar <emrecan.ayar@hotmail.com>
2022-08-20 17:58:36 +0300

    Create Context , Configurations and Constants
    Create Context , Configurations and Constants

---
+++ src/projects/rentACar/rentACar.Persistence/Configurations/Base/BaseConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Core.Persistence.ComplexTypes;
+using Core.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using rentACar.Persistence.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rentACar.Persistence.Configurations.Base
+{
+    public abstract class BaseConfiguration<T> : IEntityTypeConfiguration<T> where T : Entity
+    {
+        public void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).HasColumnName("Id").UseIdentityColumn();
+            builder.Property(x => x.Status).HasColumnName("Status").IsRequired(true).HasConversion<int>().HasDefaultValue(RecordStatu.Active);
+            builder.Property(x => x.CreatedBy).HasColumnName("CreatedBy").IsRequired(true).HasMaxLength(LengthContraints.CreatedByMaxLength).ValueGeneratedOnAdd();
+            builder.Property(x => x.ModifiedBy).HasColumnName("ModifiedBy").IsRequired(false).HasMaxLength(LengthContraints.CreatedByMaxLength);
+            builder.Property(x => x.CreatedDate).HasColumnName("CreatedDate").IsRequired(true).ValueGeneratedOnAdd();
+            builder.Property(x => x.ModifiedDate).HasColumnName("ModifiedDate").IsRequired(false);
+            builder.Property(x => x.IsDeleted).HasColumnName("IsDeleted").IsRequired(true).HasDefaultValue(false);
+            builder.HasQueryFilter(x => x.IsDeleted == false);
+        }
+    }
+}
--- src/projects/rentACar/rentACar.Persistence/Contexts/BaseDbContext.cs
+++ src/projects/rentACar/rentACar.Persistence/Contexts/BaseDbContext.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Core.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using rentACar.Persistence.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +13,18 @@ namespace rentACar.Persistence.Contexts
 {
     public class BaseDbContext : DbContext
     {
+        protected IConfiguration Configuration { get; set; }
 
+        public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.RegisterAllEntities<Entity>(Assembly.GetExecutingAssembly());
+            modelBuilder.RegisterAllConfigurations(Assembly.GetExecutingAssembly());
+        }
     }
 }
