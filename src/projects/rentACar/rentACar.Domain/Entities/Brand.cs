b3e49e82d6f56861b3ab80b20b883935fe90af27
Emre Can Ayar <emrecan.ayar@hotmail.com>
2022-08-20 17:21:05 +0300

    Create Brand Entity
    Create Brand Entity

---
+++ src/projects/rentACar/rentACar.Domain/Entities/Brand.cs
@@ -0,0 +1,32 @@
+﻿using Core.Persistence.ComplexTypes;
+using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rentACar.Domain.Entities
+{
+    public class Brand : Entity
+    {
+        public string Name { get; set; }
+
+        public Brand()
+        {
+
+        }
+
+        public Brand(int id, string name, RecordStatu status, string createdBy, DateTime createdDate, string modifiedBy, DateTime? modifiedDate, bool isDeleted) : this()
+        {
+            Id = id;
+            Name = name;
+            Status = status;
+            CreatedBy = createdBy;
+            CreatedDate = createdDate;
+            ModifiedBy = modifiedBy;
+            ModifiedDate = modifiedDate;
+            IsDeleted = isDeleted;
+        }
+    }
+}
