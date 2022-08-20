ae9d5d94d9d5f257e0d237d7a7de8e9407462fc8
Emre Can Ayar <emrecan.ayar@hotmail.com>
2022-08-20 17:16:02 +0300

    Repository Added
    Repository Added

---
+++ src/corePackages/Core.Persistence/ComplexTypes/Enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.ComplexTypes
+{
+   public enum RecordStatu
+    {
+        None = 0,
+        Active = 1,
+        Passive = 2,
+    }
+}
---
+++ src/corePackages/Core.Persistence/Repositories/Entity.cs
@@ -0,0 +1,31 @@
+﻿using Core.Persistence.ComplexTypes;
+
+namespace Core.Persistence.Repositories
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+        public RecordStatu Status { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public string ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; } = DateTime.Now;
+        public bool IsDeleted { get; set; }
+
+        public Entity()
+        {
+
+        }
+
+        public Entity(int id, RecordStatu status, string createdBy, DateTime createdDate, string modifiedBy, DateTime? modifiedDate, bool isDeleted) : this()
+        {
+            Id = id;
+            Status = status;
+            CreatedBy = createdBy;
+            CreatedDate = createdDate;
+            ModifiedBy = modifiedBy;
+            ModifiedDate = modifiedDate;
+            IsDeleted = isDeleted;
+        }
+    }
+}
