bf98ffb7115ea5195c9e3dbca9dbbdbbc93f7584
Emre Can Ayar <emrecan.ayar@hotmail.com>
2022-08-21 22:40:03 +0300

    IpSafeMiddleware Added
    IpSafeMiddleware Added

---
+++ src/corePackages/Core.Security/ApplicationSecurity/Middlewares/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Security.ApplicationSecurity.Middlewares.Extensions
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseIpSafe(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<IpSafeMiddleware>();
+        }
+    }
+}
---
+++ src/corePackages/Core.Security/ApplicationSecurity/Middlewares/IpSafeMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Core.Security.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Security.ApplicationSecurity.Middlewares
+{
+    public class IpSafeMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IpList _ipList;
+
+        public IpSafeMiddleware(RequestDelegate next, IOptions<IpList> ipList)
+        {
+            _next = next;
+            _ipList = ipList.Value;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var requestIpAddress = context.Connection.RemoteIpAddress;
+            var isWhiteList = _ipList.WhiteList.Where(x => IPAddress.Parse(x).Equals(requestIpAddress)).Any();
+            if (!isWhiteList)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                return;
+            }
+
+            await _next.Invoke(context);
+        }
+    }
+}
