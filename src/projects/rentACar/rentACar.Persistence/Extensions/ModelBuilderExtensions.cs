ba7128f8940fd5f827443db72f9aa3f48f50767a
Emre Can Ayar <emrecan.ayar@hotmail.com>
2022-10-09 19:07:48 +0300

    Update ModelBuilderExtensions.cs

--- src/projects/rentACar/rentACar.Persistence/Extensions/ModelBuilderExtensions.cs
+++ src/projects/rentACar/rentACar.Persistence/Extensions/ModelBuilderExtensions.cs
@@ -1,20 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace rentACar.Persistence.Extensions
 {
     public static class ModelBuilderExtensions
     {
         private const string _entityNamespace = "rentACar.Domain.Entities";
+        private const string _securityEntityNameSpave = "Core.Security.Entities";
         public static void RegisterAllEntities<T>(this ModelBuilder modelBuilder, params Assembly[] assemblies)
         {
             var types = assemblies.SelectMany(a => a.GetExportedTypes())
                                   .Where(c => c.IsClass && !c.IsAbstract && c.IsPublic && c.Namespace.StartsWith(_entityNamespace) && typeof(T).IsAssignableFrom(c));
+            foreach (var type in types)
+                modelBuilder.Entity(type);
+        }
+
+        public static void RegisterSecurityAllEntities<T>(this ModelBuilder modelBuilder, params Assembly[] assemblies)
+        {
+            var types = assemblies.SelectMany(a => a.GetExportedTypes())
+                                  .Where(c => c.IsClass && !c.IsAbstract && c.IsPublic && c.Namespace.StartsWith(_securityEntityNameSpave) && typeof(T).IsAssignableFrom(c));
             foreach (var type in types)
                 modelBuilder.Entity(type);
         }
