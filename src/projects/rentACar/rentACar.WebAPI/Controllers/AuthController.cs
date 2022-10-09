59472c42744a007a60af3705bafeaf254c713f82
Emre Can Ayar <emrecan.ayar@hotmail.com>
2022-10-09 21:41:57 +0300

    Create AuthController

---
+++ src/projects/rentACar/rentACar.WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using Core.Security.Dtos;
+using Core.Security.Entities;
+using Microsoft.AspNetCore.Mvc;
+using rentACar.Application.Features.Auths.Commands.Register;
+using rentACar.Application.Features.Auths.Dtos;
+using rentACar.WebAPI.Controllers.Base;
+
+namespace rentACar.WebAPI.Controllers
+{
+    public class AuthController : BaseController
+    {
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            RegisterCommand registerCommand = new()
+            {
+                UserForRegister = userForRegisterDto,
+                IpAddress = GetIpAddress()
+            };
+
+            RegisteredDto result = await Mediator.Send(registerCommand);
+            SetRefreshTokenToCookie(result.RefreshToken);
+            return Created("", result);
+        }
+
+
+        private void SetRefreshTokenToCookie(RefreshToken refreshToken)
+        {
+            CookieOptions cookieOptions = new()
+            {
+                HttpOnly = true,
+                Expires = DateTime.Now.AddDays(7)
+            };
+
+            Response.Cookies.Append("refreshToken", refreshToken.Token, cookieOptions);
+        }
+    }
+}
+
--- src/projects/rentACar/rentACar.WebAPI/Controllers/Base/BaseController.cs
+++ src/projects/rentACar/rentACar.WebAPI/Controllers/Base/BaseController.cs
@@ -9,5 +9,11 @@ namespace rentACar.WebAPI.Controllers.Base
     {
         protected IMediator? Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
         private IMediator? _mediator;
+
+        protected string? GetIpAddress()
+        {
+​           if (Request.Headers.ContainsKey("X-Forwarded-For")) return Request.Headers["X-Forwarded-For"];
+            return HttpContext.Connection.RemoteIpAddress?.MapToIPv4().ToString();
+        }
     }
 }
