cab796e7a401fe9463721746ef10d6470aca2c6d
Emre Can Ayar <emrecan.ayar@hotmail.com>
2022-10-13 18:46:51 +0300

    Update GetByTokenDocumentQuery.cs

--- src/projects/rentACar/rentACar.Application/Features/Documents/Queries/GetByTokenDocument/GetByTokenDocumentQuery.cs
+++ src/projects/rentACar/rentACar.Application/Features/Documents/Queries/GetByTokenDocument/GetByTokenDocumentQuery.cs
@@ -1,16 +1,18 @@
-﻿using MediatR;
+﻿using Core.Application.ResponseTypes.Concrete;
+using MediatR;
 using rentACar.Application.Features.Documents.Dtos;
 using rentACar.Application.Features.Documents.Rules;
 using rentACar.Application.Services.Repositories;
 using rentACar.Domain.Entities;
+using System.Net;
 
 namespace rentACar.Application.Features.Documents.Queries.GetByTokenDocument
 {
-    public class GetByTokenDocumentQuery : IRequest<DocumentDto>
+    public class GetByTokenDocumentQuery : IRequest<CustomResponseDto<DocumentDto>>
     {
         public string Token { get; set; }
 
-        public class GetByTokenDocumentQueryHandler : IRequestHandler<GetByTokenDocumentQuery, DocumentDto>
+        public class GetByTokenDocumentQueryHandler : IRequestHandler<GetByTokenDocumentQuery, CustomResponseDto<DocumentDto>>
         {
             private readonly IDocumentRepository _documentRepository;
             private readonly DocumentBusinessRules _documentBusinessRules;
@@ -21,12 +23,12 @@ namespace rentACar.Application.Features.Documents.Queries.GetByTokenDocument
                 _documentBusinessRules = documentBusinessRules;
             }
 
-            public async Task<DocumentDto> Handle(GetByTokenDocumentQuery request, CancellationToken cancellationToken)
+            public async Task<CustomResponseDto<DocumentDto>> Handle(GetByTokenDocumentQuery request, CancellationToken cancellationToken)
             {
                 Document? document = await _documentRepository.GetAsync(d => d.Token == request.Token);
                 _documentBusinessRules.DocumentShouldExistWhenRequested(document);
                 DocumentDto documentDto = ObjectMapper.Mapper.Map<DocumentDto>(document);
-                return documentDto;
+                return CustomResponseDto<DocumentDto>.Success((int)HttpStatusCode.OK, documentDto, isSuccess: true);
 
             }
         }
