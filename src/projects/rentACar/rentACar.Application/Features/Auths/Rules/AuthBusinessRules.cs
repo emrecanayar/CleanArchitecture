70059fc0c768f5916128b5e2205222bafbf09d88
Emre Can Ayar <emrecan.ayar@hotmail.com>
2022-10-15 22:06:18 +0300

    Update AuthBusinessRules.cs

--- src/projects/rentACar/rentACar.Application/Features/Auths/Rules/AuthBusinessRules.cs
+++ src/projects/rentACar/rentACar.Application/Features/Auths/Rules/AuthBusinessRules.cs
@@ -1,5 +1,7 @@
 ﻿using Core.CrossCuttingConcerns.Exceptions;
 using Core.Security.Entities;
+using Core.Security.Hashing;
+using rentACar.Application.Features.Auths.Constants;
 using rentACar.Application.Services.Repositories;
 
 namespace rentACar.Application.Features.Auths.Rules
@@ -17,6 +19,19 @@ namespace rentACar.Application.Features.Auths.Rules
         {
             User? user = await _userRepository.GetAsync(x => x.Email == email);
             if (user is not null) throw new BusinessException("Mail already exists");
+        }
+
+        public Task UserShouldBeExists(User? user)
+        {
+            if (user == null) throw new BusinessException(AuthMessages.UserDontExists);
+            return Task.CompletedTask;
+        }
+
+        public async Task UserPasswordShouldBeMatch(int id, string password)
+        {
+            User? user = await _userRepository.GetAsync(u => u.Id == id);
+            if (!HashingHelper.VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+                throw new BusinessException(AuthMessages.PasswordDontMatch);
         }
     }
 }
