64403b78cbadded62ae9c3f85b0b6baab73e5d25
Emre Can Ayar <emrecan.ayar@hotmail.com>
2022-11-03 16:32:19 +0300

    Added DeleteOperationClaimCommand

---
+++ src/projects/rentACar/rentACar.Application/Features/OperationClaims/Commands/DeleteOperationClaim/DeleteOperationClaimCommand.cs
@@ -0,0 +1,37 @@
+﻿using Core.Application.Pipelines.Authorization;
+using Core.Security.Entities;
+using MediatR;
+using rentACar.Application.Features.OperationClaims.Dtos;
+using rentACar.Application.Features.OperationClaims.Rules;
+using rentACar.Application.Services.Repositories;
+
+namespace rentACar.Application.Features.OperationClaims.Commands.DeleteOperationClaim
+{
+    public class DeleteOperationClaimCommand : IRequest<DeletedOperationClaimDto>, ISecuredRequest
+    {
+        public int Id { get; set; }
+        public string[] Roles => throw new NotImplementedException();
+
+        public class DeleteOperationClaimCommandHanlder : IRequestHandler<DeleteOperationClaimCommand, DeletedOperationClaimDto>
+        {
+            private readonly IOperationClaimRepository _operationClaimRepository;
+            private readonly OperationClaimBusinessRules _operationClaimBusinessRules;
+
+            public DeleteOperationClaimCommandHanlder(IOperationClaimRepository operationClaimRepository, OperationClaimBusinessRules operationClaimBusinessRules)
+            {
+                _operationClaimRepository = operationClaimRepository;
+                _operationClaimBusinessRules = operationClaimBusinessRules;
+            }
+
+            public async Task<DeletedOperationClaimDto> Handle(DeleteOperationClaimCommand request, CancellationToken cancellationToken)
+            {
+                await _operationClaimBusinessRules.OperationClaimIdShouldExistWhenSelected(request.Id);
+                OperationClaim mappedOperationClaim = ObjectMapper.Mapper.Map<OperationClaim>(request);
+                OperationClaim deletedOperationClaim = await _operationClaimRepository.DeleteAsync(mappedOperationClaim);
+                DeletedOperationClaimDto deletedOperationClaimDto =
+                    ObjectMapper.Mapper.Map<DeletedOperationClaimDto>(deletedOperationClaim);
+                return deletedOperationClaimDto;
+            }
+        }
+    }
+}
