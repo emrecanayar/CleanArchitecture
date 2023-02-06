907655bb9670e865d5f9955722066b783ca3ae86
Emre Can Ayar <emrecan.ayar@hotmail.com>
2023-02-06 23:53:27 +0300

    Various platforms have been configured for SeriLog.
    Various platforms have been configured for SeriLog.

--- src/corePackages/Core.CrossCuttingConcerns/Logging/SeriLog/ConfigurationModels/FileLogConfiguration.cs
+++ src/corePackages/Core.CrossCuttingConcerns/Logging/SeriLog/ConfigurationModels/FileLogConfiguration.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Core.CrossCuttingConcerns.Logging.SeriLog.ConfigurationModels
+﻿namespace Core.CrossCuttingConcerns.Logging.SeriLog.ConfigurationModels
 {
     public class FileLogConfiguration
     {
--- src/corePackages/Core.CrossCuttingConcerns/Logging/SeriLog/Logger/FileLogger.cs
+++ src/corePackages/Core.CrossCuttingConcerns/Logging/SeriLog/Logger/FileLogger.cs
@@ -2,11 +2,6 @@
 using Core.CrossCuttingConcerns.Logging.SeriLog.Messages;
 using Microsoft.Extensions.Configuration;
 using Serilog;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Core.CrossCuttingConcerns.Logging.SeriLog.Logger
 {
